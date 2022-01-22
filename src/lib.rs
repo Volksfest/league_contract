db5de62f6850ce0a7309d218f749128c26c4794a
Volksfest <s.barut@tu-bs.de>
2022-01-22 22:00:38 +0100

    Do the first method implementations
    This includes the LeagueContract and the League type.

--- src/lib.rs
+++ src/lib.rs
@@ -12,14 +12,19 @@
 //! and will need the list of the trusted accounts.
 //! Maybe the money shall be refunded?
 
+extern crate near_sdk;
+
 pub mod game_types;
 pub mod main;
 
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::LookupMap;
-use near_sdk::{near_bindgen, PanicOnDefault};
+use near_sdk::collections::Vector;
+use near_sdk::collections::{LookupMap, LookupSet};
+use near_sdk::{env, near_bindgen, require, AccountId, PanicOnDefault};
 
-use main::league::League;
+use game_types::GameType;
+use main::keys::CollectionKeyTuple;
+use main::league::{League, LeagueProperties, UpgradeableLeagueProperties};
 
 /// The smart contract
 #[near_bindgen]
@@ -27,4 +32,136 @@ use main::league::League;
 pub struct LeagueContract {
     /// A map of named leagues. The name is given by the key.
     leagues: LookupMap<String, League>,
+}
+
+#[near_bindgen]
+impl LeagueContract {
+    /// Initialize the contract
+    #[init]
+    pub fn new() -> Self {
+        require!(!env::state_exists(), "Already initialized");
+
+        Self {
+            leagues: LookupMap::new(b"0".to_vec()),
+        }
+    }
+
+    pub fn create_league(
+        &mut self,
+        league_name: &str,
+        players: &[String],
+        accounts: &[AccountId],
+        best_of: usize,
+        game_type: GameType,
+    ) {
+        require!(best_of % 2 == 1, "best_of number should be odd");
+        require!(!accounts.is_empty(), "Need at least one trusted account");
+        require!(players.len() > 2, "League needs at least 3 participant");
+        require!(
+            league_name.len() > 2,
+            "League name must be at least 3 chars long"
+        );
+        require!(
+            !self.leagues.contains_key(&league_name.to_string()),
+            "League with that name already exists"
+        );
+
+        let keys = CollectionKeyTuple::new(league_name);
+
+        let prop = UpgradeableLeagueProperties::V1(LeagueProperties { best_of, game_type });
+
+        let mut p = Vector::new(keys.get_players_key());
+        for player in players {
+            p.push(player);
+        }
+        let mut a = LookupSet::new(keys.get_trusted_key());
+        for account in accounts {
+            a.insert(account);
+        }
+        let l = League::new(keys, prop, p, a);
+        self.leagues.insert(&league_name.to_string(), &l);
+    }
+}
+
+#[cfg(all(test, not(target_arch = "wasm32")))]
+mod tests {
+    use crate::game_types::GameType::StandardGameType;
+    use crate::LeagueContract;
+    use near_sdk::test_utils::{accounts, VMContextBuilder};
+    use near_sdk::testing_env;
+
+    #[test]
+    #[should_panic(expected = "The contract is not initialized")]
+    fn test_default() {
+        let mut context = VMContextBuilder::new();
+        context
+            .current_account_id(accounts(0))
+            .signer_account_id(accounts(0))
+            .predecessor_account_id(accounts(0));
+        testing_env!(context.build());
+
+        let _contract = LeagueContract::default();
+    }
+
+    #[test]
+    fn test_new() {
+        let mut context = VMContextBuilder::new();
+        context
+            .current_account_id(accounts(0))
+            .signer_account_id(accounts(0))
+            .predecessor_account_id(accounts(0));
+        testing_env!(context.build());
+
+        let _contract = LeagueContract::new();
+    }
+
+    #[test]
+    fn test_create_league() {
+        let mut context = VMContextBuilder::new();
+        context
+            .current_account_id(accounts(0))
+            .signer_account_id(accounts(0))
+            .predecessor_account_id(accounts(0));
+        testing_env!(context.build());
+
+        let mut contract = LeagueContract::new();
+        let players = vec!["Alice".to_string(), "Bob".to_string(), "Charly".to_string()];
+        let accs = vec![accounts(0), accounts(1)];
+        contract.create_league(
+            &"SomeLeague".to_string(),
+            &players,
+            &accs,
+            3,
+            StandardGameType,
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "League with that name already exists")]
+    fn test_create_league_twice() {
+        let mut context = VMContextBuilder::new();
+        context
+            .current_account_id(accounts(0))
+            .signer_account_id(accounts(0))
+            .predecessor_account_id(accounts(0));
+        testing_env!(context.build());
+
+        let mut contract = LeagueContract::new();
+        let players = vec!["Alice".to_string(), "Bob".to_string(), "Charly".to_string()];
+        let accs = vec![accounts(0), accounts(1)];
+        contract.create_league(
+            &"SomeLeague".to_string(),
+            &players,
+            &accs,
+            3,
+            StandardGameType,
+        );
+        contract.create_league(
+            &"SomeLeague".to_string(),
+            &players,
+            &accs,
+            3,
+            StandardGameType,
+        );
+    }
 }
--- src/main/league.rs
+++ src/main/league.rs
@@ -13,16 +13,33 @@ use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use super::game_match::GameMatch;
 use crate::game_types::GameType;
 
-use near_sdk::collections::LookupMap;
 use near_sdk::collections::LookupSet;
+use near_sdk::collections::UnorderedMap;
 use near_sdk::collections::Vector;
 use near_sdk::AccountId;
+
+use near_sdk::env;
+
+use crate::main::keys::CollectionKeyTuple;
 
 /// The contestants of a `GameMatch`.
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct PlayerPair {
     first: usize,
     second: usize,
+}
+
+impl PlayerPair {
+    pub fn new(first: usize, second: usize) -> Self {
+        if first <= second {
+            PlayerPair { first, second }
+        } else {
+            PlayerPair {
+                first: second,
+                second: first,
+            }
+        }
+    }
 }
 
 /// The actual league object holding everything together.
@@ -33,9 +50,37 @@ pub struct League {
     /// The (constant) list of all participants of the league
     players: Vector<String>,
     /// The actual games between all contestants.
-    game_matches: LookupMap<PlayerPair, GameMatch>,
+    game_matches: UnorderedMap<PlayerPair, GameMatch>,
     /// The set of accounts being allowed to manipulate the league. Can be seen as moderators.
-    trusted_accounts: LookupSet<AccountId>,
+    trusted_account_ids: LookupSet<AccountId>,
+}
+
+impl League {
+    pub fn new(
+        keys: CollectionKeyTuple,
+        properties: UpgradeableLeagueProperties,
+        players: Vector<String>,
+        trusted_account_ids: LookupSet<AccountId>,
+    ) -> Self {
+        League {
+            properties,
+            players,
+            trusted_account_ids,
+            game_matches: UnorderedMap::new(keys.get_matches_key()),
+        }
+    }
+
+    pub fn is_allowed(&self) -> bool {
+        self.trusted_account_ids
+            .contains(&env::predecessor_account_id())
+    }
+
+    pub fn is_finished(&self) -> bool {
+        let p = self.players.len();
+        // Gaussian sum formula.
+        // It yields to the number of matches where each player played with everybody.
+        self.game_matches.len() == p * (p - 1) / 2
+    }
 }
 
 /// The upgradeable enum for the properties to be able to easily upgrade the league
@@ -48,7 +93,7 @@ pub enum UpgradeableLeagueProperties {
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct LeagueProperties {
     /// The maximum amount of games each `GameMatch` may have
-    best_of: usize,
+    pub best_of: usize,
     /// The actual type of the game which is played.
-    game_type: GameType,
+    pub game_type: GameType,
 }
